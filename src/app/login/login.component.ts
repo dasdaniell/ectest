b06e874c03fd6479fe28d5948a9e808d9277457d
Daniel Dascalu <danielda@fpt.com>
2024-04-25 00:37:11 +0300

    implement login page and form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public showValidationsError: boolean = false;
+  public emailAddress: string = '';
+  public password: string = '';
 
+  public valdiateCredentials() {
+    const emailRegEx = /\S+@\S+\.\S+/;
+
+    this.showValidationsError = !(this.password.length < 6 || emailRegEx.test(this.emailAddress));
+  }
 }
