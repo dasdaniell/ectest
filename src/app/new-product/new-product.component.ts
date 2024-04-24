cc055a06bd3e31caaa0a6bf06c5e2e37f0887b74
Daniel Dascalu <danielda@fpt.com>
2024-04-24 23:17:11 +0300

    create components for the reuquired app sections

---
+++ src/app/new-product/new-product.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-product',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      new-product works!
+    </p>
+  `,
+  styleUrl: './new-product.component.scss'
+})
+export class NewProductComponent {
+
+}
---
+++ src/app/sales/sales.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      sales works!
+    </p>
+  `,
+  styleUrl: './sales.component.scss'
+})
+export class SalesComponent {
+
+}
