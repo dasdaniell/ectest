6786660a1bf1c631bb94392cabb56c851306046d
Daniel Dascalu <danielda@fpt.com>
2024-04-25 16:36:37 +0300

    create sales table and map column headers

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import * as serverData from "../../data/potato_sales.json";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sales',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sales.template.html',
   styleUrl: './sales.component.scss'
 })
 export class SalesComponent {
+  public columnHedears: any[] = [];
+  public rowData: any[] = [];
 
   public ngOnInit() {
-    console.log(serverData);
+    this.columnHedears = serverData.column;
+    this.rowData = serverData.data;
+
+    // let currentRow = this.rowData[0];
+    // console.log(currentRow[this.columnHedears[1].field]);
   }
 
 }
